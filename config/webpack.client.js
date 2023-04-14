e2fbe23dc1c4dc7075c396cc356fa749fe660a14
Jas Vadgama <jasal.vadgama@wetransfer.com>
2023-04-14 17:20:28 +0100

    chore: using env to set public path

--- config/webpack.client.js
+++ config/webpack.client.js
@@ -1,15 +1,21 @@
+require('dotenv').config({ path: './.env.local' }); 
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require('webpack');
 const { merge } = require("webpack-merge");
+
 const sharedWebpackConfig = require("./webpack.shared");
 const moduleFederationPlugin = require("./module-federation");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(sharedWebpackConfig, {
   output: {
     path: path.resolve(__dirname, "../build/client"),
-    publicPath: "http://localhost:3001/client/",
+    publicPath: `${process.env.VERCEL_URL}/client/`,
   },
   plugins: [
+    new DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
     moduleFederationPlugin.client,
     new HtmlWebPackPlugin({
       template: "./public/index.html",
